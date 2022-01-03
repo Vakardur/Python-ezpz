b1cb663fc650ee44b259baa9cd80beb7f08bc50b
Ayano <s.correa.wilches@gmail.com>
2022-01-03 16:02:51 +0700

    Added comments on exercise 8

--- 2. For/Ejercicio 8.py
+++ 2. For/Ejercicio 8.py
@@ -1,7 +1,15 @@
 """Dado un número, cuente el número total de dígitos del mismo. Ejemplo, el número es 81195, por lo que la salida debería ser 5."""
 
-#¿Estamos seguros de que aquí se necesita un for?
+# ¿Estamos seguros de que aquí se necesita un for?
 
-número = int(input("Escriba un número:\n"))
-print(len(str(número)))
+numero = int(input("Escriba un número:\n"))
+print(len(str(numero)))
 
+# Se puede hacer de varias maneras, pero la idea es que practique el for
+# La idea era hacer esto ↓
+numero = str(numero)
+counter = 0
+for char in range(len(numero)):
+    counter += 1
+
+print(counter)
