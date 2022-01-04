34a47d912eeb86f5e6dfab39ef068892c7e75252
Ayano <s.correa.wilches@gmail.com>
2022-01-05 05:13:15 +0700

    Added a cat

---
+++ POO/Clases/gato.py
@@ -0,0 +1,28 @@
+import random
+
+
+class Gato:
+
+    # Atributos
+    color = "white"
+    size = 30
+    weight = 20
+    breed = "Siamese"
+    max_jump_height = 0
+
+    def jump(self):
+        jump_height = random.randint(1, 50)
+        self.max_jump_height = jump_height
+
+    def __init__(self, color):
+        self.color = color
+
+
+new_cat_1 = Gato(color='black')
+new_cat_2 = Gato(color='white')
+
+
+new_cat_1.jump()
+new_cat_2.jump()
+print(new_cat_1.max_jump_height)
+print(new_cat_2.max_jump_height)
