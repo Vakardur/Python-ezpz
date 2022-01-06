71fd99a1129b86c03432f47df76c62e9e21dc99a
Vakardur <santiagocardenase12@gmail.com>
2022-01-05 21:19:12 -0500

    Ejercicios del 3 al 5
    pain.

---
+++ 3. While/Ejercicio 5.py
@@ -0,0 +1,18 @@
+"""Siga los siguientes pasos
+        a. Ejecute {k: random.randrange(1, 1000) for k in range(100)}. El valor resultante debe ser una lista con cien elementos, del 0 al 100.
+        b. De dicha lista, imprima los números pares y que estén en la constante 0.5772156649 (Euler).
+        c. De dicha lista, imprima los números impares y que estén en la constante 3.14159 (pi).
+"""
+
+import random
+
+lista = {str(k): random.randrange(1, 1000) for k in range(100)}
+
+#for
+euler = 0.5772156649
+pi = 3.14159
+
+for x in lista[]:
+    print(x)
+
+#i am in pain.
