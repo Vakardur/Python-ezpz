7a24efb4740d03518db6db593c21fd2d54e15a73
Ayano <s.correa.wilches@gmail.com>
2022-01-21 05:08:57 +0700

    Jaja 4d

--- 4. Funciones/Ejercicio 4.py
+++ 4. Funciones/Ejercicio 4.py
@@ -4,26 +4,19 @@ otro número aleatorio de 10000. Replique la función utilizando Lambda."""
 
 import random
 
-#Función normal
+# Función normal
 
-rand_number = random.randint(1,10)
-rand_big = random.randint(1,10000)
+rand_number = random.randint(1, 10)
+rand_big = random.randint(1, 10000)
 
-def frequency (x):
-    counter = 0
-    for x in range(1,rand_big):
-        if str(rand_number) in str(rand_big):
-            counter += 1
-    return counter
 
-print(rand_number)
-print(rand_big)
-
-print(frequency(rand_number))
+# for example if rand_number is 5 and rand_big is 1554, the function will return 2
+def check_small_in_big():
+    return
 
 
 """Ok, no sé si la lógica que estoy implementando tiene sentido o no. Siempre me imprime 0. """
 
-#Función lambda
+# Función lambda
 
 """pending"""
