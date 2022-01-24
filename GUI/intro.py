6db65c8a8a62714a5ab10e09f0885aa35b4b8372
Vakardur <santiagocardenase12@gmail.com>
2022-01-24 01:37:08 -0500

    Attempt at creating bread sale.

--- GUI/intro.py
+++ GUI/intro.py
@@ -6,8 +6,10 @@ Tkinter > JavaScript->React > C++ > Unity
 
 # EN ESTE ARCHIVO SOLO VAN COSAS RELACIONADAS A LA INTERFAZ GRAFICA
 
-from tkinter import Button, Entry, Tk, Label
+from tkinter import *
+import tkinter
 import tkinter.font as font
+from turtle import color, width
 
 
 # Init
@@ -18,27 +20,34 @@ root.resizable(False, False)
 root.configure(background="white")
 # root.iconbitmap('icon.ico') dentro de la carpeta de GUI
 
-
 # font config
 arial = font.Font(family="Arial", size=20)
-
 
 # title
 title = Label(root, text="Tiendita de la esquina", background="white")
 title['font'] = arial
 title.pack()
 
-
+def new_entry():
+    new_entry = Entry(root, width = 15, background='#f9a9f9')
+    new_entry.pack()
+def confirmbutton():
+    newbtn = Button(root, text= 'Confirm', width=100, height=50)
+    newbtn.pack()
+def sale_btn():
+        
 # button
 # command es lo que ejecutará el botón al darle click
-button = Button(root, text="Click me", background="white",
-                command=lambda x: print('hola'), width=20, height=10)
-button['font'] = arial
+pan_img = PhotoImage(file=r'GUI\pan.png')
+pan_btn = pan_img.subsample(10,10)
+SaleButton = Button(root, image= pan_btn,
+                command=lambda x:  width=100, height=50)
+SaleButton['font'] = arial
 
 # entry
 data_entry = Entry(root, width=20, background="#f9a9f9")
-button.pack()
-data_entry.pack()
+SaleButton.pack()
+
 
 root.bind('<space>', lambda x: print('hola con espacio'))
 
@@ -49,7 +58,8 @@ if __name__ == "__main__":
 """
 VENDA UN ARTICULO
 
-Ponga un botón con una imagen adentro de un pan. Si le doy click al pan aparece una entrada de texto y un botón de confirmar.
+Ponga un botón con una imagen adentro de un pan. Si le doy click al pan aparece 
+una entrada de texto y un botón de confirmar.
 
 Si le doy click a confirmar, por consola imprima el nuevo stock del artículo.
 Empiece con:
